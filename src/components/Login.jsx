cedbd5754e11b855d88064800fdd261c5c1a3d2c
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2023-09-17 17:39:54 +0530

    added integration for login user api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState({});
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -14,9 +17,25 @@ function Login() {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // server
+    try {
+      const response = await axios.post('http://localhost:3002/api/signin', {
+        username: formData.email,
+        password: formData.password,
+      });
+
+      if (response.status === 200) {
+        setLoggedInUser(response.data.user);
+        setLoginSuccess(true);
+        console.log('Login successful', response.data.user);
+      } else {
+        console.error('Login failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
@@ -27,36 +46,42 @@ function Login() {
             Back
           </Link>
         </div>
-        <div>
-          <form
-            className='p-8 bg-white shadow-lg rounded'
-            onSubmit={handleSubmit}
-          >
-            <h2 className='text-2xl mb-4'>Login</h2>
+        {!loginSuccess ? (
+          <div>
+            <form
+              className='p-8 bg-white shadow-lg rounded'
+              onSubmit={handleSubmit}
+            >
+              <h2 className='text-2xl mb-4'>Login</h2>
 
-            <input
-              className='mb-4 w-full p-2 border rounded'
-              type='email'
-              placeholder='Email'
-              name='email'
-              value={formData.email}
-              onChange={handleChange}
-            />
+              <input
+                className='mb-4 w-full p-2 border rounded'
+                placeholder='Email'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+              />
 
-            <input
-              className='mb-4 w-full p-2 border rounded'
-              type='password'
-              placeholder='Password'
-              name='password'
-              value={formData.password}
-              onChange={handleChange}
-            />
+              <input
+                className='mb-4 w-full p-2 border rounded'
+                type='password'
+                placeholder='Password'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+              />
 
-            <button className='w-full px-4 py-2 bg-green-500 text-white rounded'>
-              Login
-            </button>
-          </form>
-        </div>
+              <button className='w-full px-4 py-2 bg-green-500 text-white rounded'>
+                Login
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className='p-8 bg-white shadow-lg rounded'>
+            <h2 className='text-2xl mb-4'>Login Successful!</h2>
+            <p>{`You are now logged in ${loggedInUser.userName}.`}</p>
+          </div>
+        )}
       </div>
     </>
   );
